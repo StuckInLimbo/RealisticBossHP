ab8f76e92cb9b8268671c5ea92efb088216679f7
StuckInLimbo <thomas@limbofps.net>
2025-10-27 16:36:35 -0400

    Update to SPT 4.0.X

---
+++ Config.cs
@@ -0,0 +1,21 @@
+namespace RealisticBossHP;
+
+internal class Config
+{
+	public bool raider_Enabled { get; set; }
+	public double raider_Head { get; set; }
+	public double raider_Chest { get; set; }
+	public double raider_Stomach { get; set; }
+	public double raider_LeftArm { get; set; }
+	public double raider_RightArm { get; set; }
+	public double raider_LeftLeg { get; set; }
+	public double raider_RightLeg { get; set; }
+	public bool boss_Enabled { get; set; }
+	public double boss_Head { get; set; }
+	public double boss_Chest { get; set; }
+	public double boss_Stomach { get; set; }
+	public double boss_LeftArm { get; set; }
+	public double boss_RightArm { get; set; }
+	public double boss_LeftLeg { get; set; }
+	public double boss_RightLeg { get; set; }
+}
