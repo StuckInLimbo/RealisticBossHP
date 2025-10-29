03208fd572ab786eef0b3128c61ca950bc5d115b
StuckInLimbo <thomas@limbofps.net>
2025-10-29 02:31:18 -0400

    Update author name to reflect GitHub

--- RealisticBossHPMetadata.cs
+++ RealisticBossHPMetadata.cs
@@ -6,7 +6,7 @@ public record ModMetadata : AbstractModMetadata
 {
 	public override string ModGuid { get; init; } = "net.limbofps.realisticbosshp";
 	public override string Name { get; init; } = "RealisticBossHP";
-	public override string Author { get; init; } = "LimboFPS";
+	public override string Author { get; init; } = "StuckInLimbo";
 	public override List<string>? Contributors { get; init; }
 	public override SemanticVersioning.Version Version { get; init; } = new("2.0.0");
 	public override SemanticVersioning.Range SptVersion { get; init; } = new("~4.0.0");
